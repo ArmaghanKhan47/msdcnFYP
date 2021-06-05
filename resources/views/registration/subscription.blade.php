ab1b7c8e10e9dc9d52717f14df170757a676c427
Abdullah Iqbal <ai3449713@gmail.com>
2021-06-05 17:39:09 +0500

    Local saving changes

--- resources/views/registration/subscription.blade.php
+++ resources/views/registration/subscription.blade.php
@@ -3,14 +3,14 @@
     <div class="row">
         @if (count($data) > 0)
             @foreach ($data as $package)
-                <div class="col-md-4">
-                        <div class="jumbotron">
-                            <span class="h3 d-block">{{$package->PackageName}}</span>
+                <div class="col-md-4 justify-content-center">
+                        <div class="jumbotron jumbotron_apnd text-white justify-content-center">
+                            <span class="h3 d-block text-white">{{$package->PackageName}}</span>
                             <hr>
-                            <span class="h6 d-block text-muted">{{$package->PackagePrice}} PKR</span>
-                            <span class="h6 d-block text-muted">Last {{$package->PackageDuration}} Month(s)</span>
-                            <span class="h6 d-block text-muted">Mobile App: @if($package->supportApi){{"YES"}}@else{{"NO"}}@endif</span>
-                            <a class="btn btn-secondary" href="{{route('subscription.show', [$package->PackageId])}}">Subscribe</a>
+                            <span class="h6 d-block text-white ">{{$package->PackagePrice}} PKR</span>
+                            <span class="h6 d-block text-white">Last {{$package->PackageDuration}} Month(s)</span>
+                            <span class="h6 d-block text-white">Mobile App: @if($package->supportApi){{"YES"}}@else{{"NO"}}@endif</span>
+                            <a class="btn btn-danger" href="{{route('subscription.show', [$package->PackageId])}}">Subscribe</a>
                         </div>
                 </div>
             @endforeach
--- resources/views/registration/subscriptioncheckout.blade.php
+++ resources/views/registration/subscriptioncheckout.blade.php
@@ -3,12 +3,12 @@
     <div class="row">
         <div class="col-md-6">
             {{-- Package Details --}}
-            <div class="jumbotron">
+            <div class="jumbotron jumbotron_apnd text-white">
                 <span class="h3 d-block">{{$package[0]->PackageName}}</span>
                 <hr>
-                <span class="h6 d-block text-muted">{{$package[0]->PackagePrice}} PKR</span>
-                <span class="h6 d-block text-muted">Last {{$package[0]->PackageDuration}} Month(s)</span>
-                <span class="h6 d-block text-muted">Mobile App: @if($package[0]->supportApi){{"YES"}}@else{{"NO"}}@endif</span>
+                <span class="h6 d-block ">{{$package[0]->PackagePrice}} PKR</span>
+                <span class="h6 d-block ">Last {{$package[0]->PackageDuration}} Month(s)</span>
+                <span class="h6 d-block ">Mobile App: @if($package[0]->supportApi){{"YES"}}@else{{"NO"}}@endif</span>
                 <hr>
                 <div>
                     <ul>
@@ -18,7 +18,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-6 jumbotron_apnd">
 
             @include('paymentmethod.paymentmethods', [
                 'purpose' => 'subscriptioncheckout',
