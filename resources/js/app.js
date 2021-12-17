543a484dad4cef34dbabca402afff505caac3f80
ArmaghanKhan <khanhassan918@ymail.com>
2021-12-17 11:17:49 +0500

    Initial Conversion Setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,17 @@ require('popper.js');
 require('chart.js');
 require('aos');
 require('swiper');
+
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/inertia-vue3';
+
+createInertiaApp({
+    resolve: name => require(`./pages/${name}`),
+    setup({el, App, props, plugin}){
+        createApp({
+            render: () => h(App, props)
+        })
+        .use(plugins)
+        .mount(el)
+    }
+});
